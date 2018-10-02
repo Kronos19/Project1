a4553f3c2dc9bdef04175ba99978544cd3ee3c38
M Ryan <mxryan@live.com>
2018-10-02 11:08:50 -0400

    attempt2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
     var zip = $("#zip").val().trim();
     var city = $("#city").val().trim();
     var state = $("#state").val().trim();
-    
     ticketMasterFetch(zip, city, state);
   });
 
@@ -27,7 +26,6 @@ $(document).ready(function () {
     var req = new XMLHttpRequest();
     req.onload = () => {
       var json = JSON.parse(req.responseText);
-      console.log(json);
       if (!json._embedded) {
         err("try a different zip");
         return false;
@@ -71,6 +69,7 @@ $(document).ready(function () {
         }
 
       }
+      // console.log(eventsOut);
       displayEvent(eventsOut);
       yelp(city);
     }
@@ -79,9 +78,16 @@ $(document).ready(function () {
 
   }
 
-  function yelp(arg) {
-    
-    arg = arg || console.log("no restaurant");
+  function yelp(arg, arg2, arg3, arg4) {
+    //make arguements optional
+    //zip
+    arg = arg || console.log("no zip");
+    //location
+    arg2 = arg2 || console.log("no location");
+    //lat
+    arg3 = ((arg3 || console.log("no lat")) && (arg4 || console.log("no lon")));
+    //lon
+    arg4 = ((arg4 || console.log("no lon")) && (arg3 || console.log("no lat")));
 
     //query api
     let queryURL = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=" + arg + "&radius=8046&limit=10";
@@ -99,7 +105,6 @@ $(document).ready(function () {
     }).then(function (response) {
       // console.log(response);
       let businessArr = response.businesses;
-      console.log(response);
 
       arrayRest = [];
 
@@ -131,7 +136,6 @@ $(document).ready(function () {
 
       var eventDiv = $("<div>");
       eventDiv.addClass("event");
-      eventDiv.attr('id', "events")
 
       var p = $("<p>").text("Name: " + name);
       var g = $("<p>").text("Genre: " + genre);
@@ -147,11 +151,10 @@ $(document).ready(function () {
   function restaurantButton(arrayRest) {
     $("#button-view").empty();
     for (var i = 0; i < arrayRest.length; i++) {
-      var newbutton = $("<a>");
+      var newbutton = $("<button>");
       newbutton.addClass("btn btn-md btn-danger btn-block");
       newbutton.text(arrayRest[i].name);
-      newbutton.attr("href", arrayRest[i].url);
-      newbutton.attr("id", "button")
+      newbutton.append(arrayRest[i]);
       $("#button-view").append(newbutton);
     }
   }
@@ -159,7 +162,6 @@ $(document).ready(function () {
 
   function clear() {
     $("#events-div").empty();
-    $("#button-view").empty();
     $("#zip").val("");
   }
 
