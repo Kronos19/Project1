441730247baa8ceb32490c964bfc35bf8450a5f5
M Ryan <mxryan@live.com>
2018-10-02 11:52:20 -0400

    added separate card for restaurant results

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -82,27 +82,19 @@ $(document).ready(function () {
   function yelp(arg) {
 
     arg = arg || console.log("no restaurant");
-
-    //query api
-    let queryURL = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=" + arg + "&radius=8046&limit=10";
-    //corsURL issue code fix
+    let queryURL = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=" + arg 
+      + "&radius=8046&limit=10";
     var corsURL = "https://cors-anywhere.herokuapp.com/" + queryURL;
-
     $.ajax({
       url: corsURL,
       method: "GET",
-
       headers: {
-        //my API
         'Authorization': "Bearer -fQflxBoof3K42D5NlSTEm1zb2Q4pYrNwqMIXOqoLAc2vw9W5KExghydPZmUeCQWOPFm5mRoQ9dYYIJHu1lz0_eTCuD0Aux8tZaVhM3XvD8x22mQaodNSGx5DVSuW3Yx"
       }
     }).then(function (response) {
       // console.log(response);
       let businessArr = response.businesses;
-
       arrayRest = [];
-
-      //loop the array from yelp, take the properties needed and add to a new object, and loop them all into a new array
       for (let i = 0; i < businessArr.length; i++) {
         let businessObject = businessArr[i];
         let indivRest = {};
